ced96c9754f1a210bbbde46e11959b627e1af9c2
Kishan Dev <kishandev2509@gmail.com>
2025-08-05 22:30:47 +0530

    Update 3477_Fruits_Into_Baskets_II.py

--- 3477_Fruits_Into_Baskets_II.py
+++ 3477_Fruits_Into_Baskets_II.py
@@ -1,19 +1,19 @@
-# 3479. Fruits Into Baskets III
+# 3477. Fruits Into Baskets II
 
-
-def numOfUnplacedFruits(fruits: list[int], baskets: list[int]) -> int:
-    visited = [False] * len(baskets)
-    unplaced = 0
-    for fruit in fruits:
-        placed = False
-        for i in range(len(baskets)):
-            if not visited[i] and baskets[i] >= fruit:
-                visited[i] = True
-                placed = True
-                break
-        if not placed:
-            unplaced += 1
-    return unplaced
+class Solution:
+    def numOfUnplacedFruits(self, fruits: list[int], baskets: list[int]) -> int:
+        visited = [False] * len(baskets)
+        unplaced = 0
+        for fruit in fruits:
+            placed = False
+            for i in range(len(baskets)):
+                if not visited[i] and baskets[i] >= fruit:
+                    visited[i] = True
+                    placed = True
+                    break
+            if not placed:
+                unplaced += 1
+        return unplaced
 
 
 def main():
@@ -21,8 +21,9 @@ def main():
         [[3, 6, 1], [6, 4, 7]],
         [[4, 2, 5], [3, 5, 4]],
     ]
+    solution = Solution()
     for test in tests:
-        print(f"{test} = {numOfUnplacedFruits(test[0], test[1])}")
+        print(f"{test} = {solution.numOfUnplacedFruits(test[0], test[1])}")
 
 
 if __name__ == "__main__":
