be06c4aee1b6c1f856837d0e5c2198978c3bc010
Kishan Dev <kishandev2509@gmail.com>
2025-08-09 10:28:00 +0530

    Update 231_Power_of_Two.py

--- 231_Power_of_Two.py
+++ 231_Power_of_Two.py
@@ -1,9 +1,7 @@
 # 231. Power of Two
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
-            return False
-        return n & (n - 1) == 0
+        return n > 0 and n & (n - 1) == 0
 
 
 def main():
